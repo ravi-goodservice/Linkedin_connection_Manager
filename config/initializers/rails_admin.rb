770dd2fef234ab7dead351eff43721b2a239e3af
Ravi <ravi@goodservice.com>
2017-11-16 06:10:24 +0530

    adding some changes

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,12 +1,12 @@
 RailsAdmin.config do |config|
-  config.excluded_models << User
+  #config.excluded_models << User
   config.model 'Linkedinconnectprofile' do
     # object_label_method do
     #   :custom_label_method
     # end
     list do
       # simply adding fields by their names (order will be maintained)
-      include_fields :id,:linkedinid, :created_at,:ConnectionSentByAccount,:is_scraped
+      #include_fields :id,:linkedinid, :created_at,:ConnectionSentByAccount,:is_scraped
     end
   end
   config.actions do
